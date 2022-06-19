f79cd27d584272b3d766974940306cbd316778f5
vero <veronica.delacruz12@hotmail.com>
2022-06-19 13:05:56 -0500

    Feat: add the one component

--- src/App.js
+++ src/App.js
@@ -103,14 +103,6 @@ function App() {
 		updateState();
 	};
 
-	return (
-		<div className="app">
-			<div className="container">
-				<ResultView history={state.history} output={state.displayValue} />
-				<Keyboard onClick={onClick} />
-			</div>
-		</div>
-	);
-}
+
 
 export default App;
